d6c0bb56b749075c3b2c7ab8cec2866e8b38a8c3
Love Hörnquist Åstrand <lha@kth.se>
2008-04-13 18:34:15 +0200

    test not read yet.

--- run-tests.sh
+++ run-tests.sh
@@ -1,3 +1,8 @@
 #!/bin/sh
 
 ./git2svn /Users/lha/src/heimdal/git-trunk repro
+
+#./git2svn \
+#    --svn-prefix=branches/heimdal-1-1-branch \
+#    --git-branch=heimdal-1-1-branch \
+#    /Users/lha/src/heimdal/git-trunk repro

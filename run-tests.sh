65c96e90193e48fc519cab603c9d24dfce04b22b
Love Hörnquist Åstrand <lha@kth.se>
2008-04-16 13:20:08 +0200

    some more tests

--- run-tests.sh
+++ run-tests.sh
@@ -1,10 +1,14 @@
 #!/bin/sh
 
+rm -rf repro
+
+echo "####full import of master -> trunk"
 ./git2svn \
      --verbose \
      --no-unlink \
      /Users/lha/src/heimdal/git-trunk repro || exit 1
 
+echo "####full import of branch -> branches/heimdal-1-1-branch"
 ./git2svn \
      --verbose \
      --no-unlink \
@@ -12,11 +16,19 @@
      --git-branch=heimdal-1-1-branch \
      /Users/lha/src/heimdal/git-trunk repro || exit 1
 
-# try incremental
+echo "####incremental import of master -> trunk"
 ./git2svn \
      --verbose \
      --no-unlink \
     /Users/lha/src/heimdal/git-trunk repro || exit 1
 
+echo "####test that repro looks ok at a glance"
+
+svn=file://`pwd`/repro
+
+svn ls $svn | grep trunk > /dev/null || exit 1
+svn ls $svn/branches | grep heimdal-1-1-branch > /dev/null || exit 1
+
 echo "all tests passed"
+
 exit 0

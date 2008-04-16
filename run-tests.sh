2eafc6ea4a8398bb4971b7fe3bc240e1ade577b9
Love Hörnquist Åstrand <lha@kth.se>
2008-04-16 10:03:10 +0200

    Tests.

--- run-tests.sh
+++ run-tests.sh
@@ -1,8 +1,22 @@
 #!/bin/sh
 
-./git2svn /Users/lha/src/heimdal/git-trunk repro
+./git2svn \
+     --verbose \
+     --no-unlink \
+     /Users/lha/src/heimdal/git-trunk repro || exit 1
 
-#./git2svn \
-#    --svn-prefix=branches/heimdal-1-1-branch \
-#    --git-branch=heimdal-1-1-branch \
-#    /Users/lha/src/heimdal/git-trunk repro
+./git2svn \
+     --verbose \
+     --no-unlink \
+     --svn-prefix=branches/heimdal-1-1-branch \
+     --git-branch=heimdal-1-1-branch \
+     /Users/lha/src/heimdal/git-trunk repro || exit 1
+
+# try incremental
+./git2svn \
+     --verbose \
+     --no-unlink \
+    /Users/lha/src/heimdal/git-trunk repro || exit 1
+
+echo "all tests passed"
+exit 0
